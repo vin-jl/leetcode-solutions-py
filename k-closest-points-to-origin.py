a6735cce3b5840173c62656d105762f7e3d2fa5f
Vincent Lin <vincentln520@gmail.com>
2025-11-02 20:02:40 -0500

    Update k-closest-points-to-origin.py

--- k-closest-points-to-origin.py
+++ k-closest-points-to-origin.py
@@ -1,6 +1,6 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        points.sort(key=lambda point : sqrt((point[0] - 0)**2 + (point[1] - 0) ** 2))
+        points.sort(key=lambda point : ((point[0] - 0) ** 2 + (point[1] - 0) ** 2) ** 0.5)
         """
         result = []
         for i in range(k):
